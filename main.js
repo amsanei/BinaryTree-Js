b84f5a215255fb26a1ddb2cf370717ecb3c01a04
Amir mohammad <am.sanei79@gmail.com>
2021-01-06 16:30:31 +0330

    added traverse funcs and height func

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ class Tree{
     insert(value){
         var node = new Node(value);
         if(this.root == null){
-            console.log("root");
             this.root = node;
             return;
         }
@@ -33,8 +32,47 @@ class Tree{
             }
         }
     }
+    find(value){
+        var curr = this.root;
+        while(curr != null){
+            if(value < curr.value)
+                curr = curr.leftChild;
+            else if(value > curr.value)
+                curr = curr.rightChild;
+            else
+                return true;
+        }
+        return false;
+    }
+    traversePreOrder(root = tree.root){
+        if(root == null)
+            return;
+        console.log(root.value);
+        this.traversePreOrder(root.leftChild);
+        this.traversePreOrder(root.rightChild);
+    }
+    traverseInOrder(root = tree.root){
+        if(root == null)
+            return;
+        this.traversePreOrder(root.leftChild);
+        console.log(root.value);
+        this.traversePreOrder(root.rightChild);
+    }
+    traversePostOrder(root = tree.root){
+        if(root == null)
+            return;
+        this.traversePreOrder(root.leftChild);
+        this.traversePreOrder(root.rightChild);
+        console.log(root.value);
+    }
+    height(root = this.root){
+        if(root == null)
+            return -1;
+        if(root.leftChild == null && root.rightChild == null)
+            return 0;
+        return 1 + Math.max(this.height(root.leftChild),this.height(root.rightChild));
+    }
 }
-
 var tree = new Tree;
 tree.insert(7);
 tree.insert(4);
@@ -43,4 +81,9 @@ tree.insert(1);
 tree.insert(6);
 tree.insert(8);
 tree.insert(10);
-console.log("done");
+// console.log(tree.find(10));
+// console.log(tree.find(11));
+// tree.traversePreOrder();
+// tree.traverseInOrder();
+// tree.traversePostOrder();
+console.log(tree.height());
